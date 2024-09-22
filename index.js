d33b00b7f0d722bf8d9130f564ce31057dd07138
Pooja-Manikandann <Pooja.Manikandan@cdw.com>
2024-09-22 20:30:52 +0530

    latest code

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const authRoutes = require("./routers/authRoutes");
 const movieRoutes = require("./routers/movieRoutes");
-const errorHandler = require("./middleware/errorHandler");
-const { authoriseUser } = require("./middleware/authoriseUser");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use("/", authRoutes);
-app.use("/movies", authoriseUser, movieRoutes);
-app.use(errorHandler);
+app.use("/movies", movieRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`App is listening at port ${process.env.PORT}`);
